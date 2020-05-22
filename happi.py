0e9612715ad37625af1eee1f91be98045a67b932
Gogler2828 <atmucoyyso2828@gmail.com>
2020-05-23 00:47:01 +0900

    master

--- happi.py
+++ happi.py
@@ -4,47 +4,45 @@ import time
 
 with open("tokens/happi_token.txt") as tkn:
     TOKEN = tkn.read()
-
     client = discord.Client()
 
     @client.event
     async def on_ready():
-        print("ハッピッピくんログイン中…"
+        print("ハッピッピくんログイン中…")
+        await client.change_presence(activity = discord.Game(name = "起爆コマンド待機中...",type = 1))
 
     @client.event
     async def on_message(message):
         cnt = 0
         if message.content == "!bye":
-            print("送信停止中...")
+            await client.change_presence(activity = discord.Game(name = "緊急停止信号受信・停止中..."))
             time.sleep(60)
             await message.channel.send("お騒がせしました。")
             print("送信停止解除")
-            break
+            await client.change_presence(activity = discord.Game(name = "起爆コマンド待機中..."))
+            return
         elif message.content == "/happy":
             if (random.randrange(0,40,4) % 3 == 0) or (random.randrange(0,40,4) % 3 == 1):
-                while cnt <= 10:
+                while cnt <= 5:
                     await message.channel.send("/happy")
                     cnt += 1
-                break
-
             else:
                 await message.channel.send("!5cho")
                 await message.channel.send("!spc")
                 while cnt <= 5:
                     await message.channel.send("/happy")
                     cnt += 1
-                break
-
         elif message.content == ("/stop"):
             if random.randrange(100) == 28:
+                await client.change_presence(activity = discord.Game(name = "緊急停止信号受信・停止中..."))
                 print("送信停止中...")
                 time.sleep(60)
                 await message.channel.send("お騒がせしました。")
                 print("送信停止解除")
-                break
+                await client.change_presence(activity = discord.Game(name = "起爆コマンド待機中...",type = 1))
             else:
-                while cnt <= 10:
+                while cnt <= 5:
                     await message.channel.send("/happy")
-                break
+                    cnt += 1
         
     client.run(TOKEN)
